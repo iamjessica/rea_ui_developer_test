b125b70e7b6991db59dc2ddd980ca632f81abd49
Jessica <hellojessicagraham@gmail.com>
2018-05-29 16:32:21 +1000

    added empty state

--- src/components/PropertyGrid/index.js
+++ src/components/PropertyGrid/index.js
@@ -5,15 +5,19 @@ export default class PropertyGrid extends React.Component {
     render() {
         const className = this.props.saved ? "saved" : "results";
         const buttonText = this.props.saved ? "Remove" : "Save";
+
+        let contents;
+        if (!this.props.properties.length) contents = <span>No properties</span>;
+        else contents = this.props.properties.map(property => 
+            <PropertyGridTile 
+            key={property.id} 
+            {...property} 
+            buttonText={buttonText}
+            handleClick={this.props.handleClick}
+        />);
         
         return <div className={`propertyGrid ${this.props.className || ''}`}>
-            {this.props.properties.map(property => 
-                <PropertyGridTile 
-                key={property.id} 
-                {...property} 
-                buttonText={buttonText}
-                handleClick={this.props.handleClick}
-            /> )}
+            {contents}
         </div>
     }
     
